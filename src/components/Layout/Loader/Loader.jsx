1202feaef30c70c8293a90a63ce3be7c6e0d32e4
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2023-12-24 21:43:56 +0530

    update Loader.jsx

--- src/components/Layout/Loader/Loader.jsx
+++ src/components/Layout/Loader/Loader.jsx
@@ -1,16 +1,21 @@
 import { Spinner, VStack } from '@chakra-ui/react';
 import React from 'react';
 
-const Loader = ({ color = 'yellow.500' }) => {
+const Loader = ({ color = 'purple.500' }) => {
   return (
     <VStack h="100vh" justifyContent={'center'}>
       <div style={{ transform: 'scale(4)' }}>
         <Spinner
-          thickness="2px"
+        thickness='3.5px'
+        speed='0.65s'
+        emptyColor='gray.200'
+        color={color}
+        size='xl'
+          /*thickness="2px"
           speed="0.65s"
           emptyColor="transparent"
           color={color}
-          size="xl"
+          size="xl"*/
         />
       </div>
     </VStack>
