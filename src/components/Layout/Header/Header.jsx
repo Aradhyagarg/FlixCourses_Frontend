24ffab94736b9fcde5f6ab9776d409686cf9ebbe
Aradhya Garg <aradhya0107.be20@chitkara.edu.in>
2024-01-01 22:19:25 +0530

    3 Update In Header.jsx

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -36,7 +36,7 @@ const Header = ({ isAuthenticated = false, user }) => {
       <ColorModeSwitcher />
       <Button
         onClick={onOpen}
-        color={'purple.100'}
+        color={'purple.400'}
         width="12"
         height={'12'}
         rounded="full"
